4f5202997802651223927e62003bc1af4a1e63af
Broccoli <vovo.r@ya.ru>
2024-01-10 01:01:23 +1000

    create class capitan

---
+++ shipping_company/captain.py
@@ -0,0 +1,13 @@
+class Captain:
+
+    def __init__(self,
+                 name: str,
+                 surname: str,
+                 experience: int,
+                 qualification: str
+                 ) -> None:
+
+        self.name = name  # имя капитана
+        self.surname = surname  # фамилия капитана
+        self.experience = experience  # опыт капитана
+        self.qualification = qualification  # квалификация капитана
